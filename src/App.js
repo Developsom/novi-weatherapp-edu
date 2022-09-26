0dcfe6585eee94d434a57480a4dcee11d7a40660
Developsom <zana@cleancflo.nl>
2022-09-26 14:44:22 +0700

    practise makes perfect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from "axios";
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
 import './App.css';
 
+const apiKey ='439ae5cd6506863c6415db63b81c50fd';
+
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+
+  async function fetchData() {
+    try {
+      const result = await
+          axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+      console.log(result.data);
+      setWeatherData(result.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
   return (
     <>
       <div className="weather-container">
@@ -14,11 +29,14 @@ function App() {
           <SearchBar/>
 
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
-
-            <button type="button">
+            {Object.keys(weatherData).length > 0 &&
+                <>
+                  <h2>{weatherData.weather[0].description}</h2>
+                  <h3>{weatherData.name}</h3>
+                  <h1>{weatherData.main.temp}</h1>
+                </>
+            }
+            <button type="button" onClick={fetchData}>
               Haal data op!
             </button>
           </span>
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css';
 
+
 function SearchBar() {
+    const [query, setQuery] = useState('')
+
+    function onFormSubmit() {
+
+    }
+
   return (
     <form className="searchbar">
       <input
         type="text"
         name="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
         placeholder="Zoek een stad in Nederland"
       />
 
-      <button type="submit">
+      <button
+          type="submit"
+      >
         Zoek
       </button>
     </form>
